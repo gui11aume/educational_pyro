f3112dc10ab1c80a017b681f53816bc7f4a1a71d
gui11aume <guillaume.filion@gmail.com>
2020-08-10 02:22:44 -0400

    Fix Luria-Delbruck experiment

--- luria_delbruck.py
+++ luria_delbruck.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import numbers
-import os
 import pyro
 import torch
 
@@ -11,7 +9,6 @@ import pyro.distributions as dist
 from pyro.infer import Trace_ELBO, TraceEnum_ELBO, JitTraceEnum_ELBO, SVI
 from pyro.optim import Adam
 
-smoke_test = ('CI' in os.environ)
 pyro.enable_validation()
 pyro.set_rng_seed(123)
 
@@ -39,66 +36,51 @@ def precompute_convolutions(maxp, maxlen):
    # probabilities (the value is otherwise not used).
    return torch.cat([conv, 1-conv.sum(dim=1, keepdim=True)], 1)
 
-
 DISTR = precompute_convolutions(MAXP, MAXLEN)
 
+# Truncated Poisson (up to and including MAXP).
+poisson = (lambda lmbd: torch.distributions.Poisson(lmbd)
+   .log_prob(torch.arange(MAXP+1.)).exp())
 
-def model():
+
+def model(x):
    # Half-Cauchy prior on the mutation rate.
    lmbd = pyro.sample('lmbd', dist.HalfCauchy(torch.ones(1)))
-   z = pyro.sample(
-         name = 'z',
-         fn = dist.Categorical(torch.tensor([torch.exp(-lmbd), 1.-torch.exp(-lmbd)])),
-         infer = {'enumerate': 'parallel'}
-   )
-   import pdb; pdb.set_trace()
-#   # Truncated Poisson (up to and including MAXP).
-#   logits = torch.distributions.Poisson(lmbd).log_prob(torch.arange(MAXP+1.))
-#   z = pyro.sample(
-#         name = 'z',
-#         fn = dist.Categorical(logits=logits),
-#         infer = { 'enumerate': 'parallel' }
-#   )
-   with pyro.plate('plate_x'):
+   probs = poisson(lmbd)
+   with pyro.plate('plate_x', x.shape[0]):
+      # Number of mutations.
+      z = pyro.sample(
+           name = 'z',
+           fn = dist.Categorical(probs=probs),
+           infer = {'enumerate': 'parallel'}
+      )
+      # Number of resistant individuals.
       x = pyro.sample(
-            name = 'x',
-            fn = dist.Categorical(DISTR[z,:])
+           name = 'x',
+           fn = dist.Categorical(DISTR[z,:]),
+           obs = x
       )
 
-      
-def guide():
+
+def guide(x):
    # Model 'lmbd' as a LogNormal variate.
    a = pyro.param('a', torch.zeros(1))
    b = pyro.param('b', torch.ones(1),
          constraint=torch.distributions.constraints.positive)
    lmbd = pyro.sample('lmbd', dist.LogNormal(a,b))
-#   # Truncated Poisson (up to and including MAXP).
-#   logits = torch.distributions.Poisson(lmbd).log_prob(torch.arange(MAXP+1.))
-#   z = pyro.sample(
-#         name = 'z',
-#         fn = dist.Categorical(logits=logits),
-#         infer = { 'enumerate': 'parallel' }
-#   )
-   z = pyro.sample(
-         name = 'z',
-         fn = dist.Categorical(torch.tensor([torch.exp(-lmbd), 1.-torch.exp(-lmbd)])),
-         infer = {'enumerate': 'parallel'}
-   )
 
 
-# Data from the Luria-Delbruck experiment.
-#obs = torch.tensor([1,0,3,0,0,5,0,5,0,6,107,0,0,0,1,0,0,64,0,35])
-obs = torch.ones(20)
-cnd_model = pyro.poutine.condition(model, data={ 'x': obs })
+# Data from the Luria-Delbruck experiment (resistant individuals).
+obs = torch.tensor([1,0,3,0,0,5,0,5,0,6,107,0,0,0,1,0,0,64,0,35])
 
 
 optimizer = Adam({ 'lr': 0.05 })
-ELBO = JitTraceEnum_ELBO(max_plate_nesting=1)
-svi = SVI(cnd_model, guide, optimizer, ELBO)
+ELBO = JitTraceEnum_ELBO()
+svi = SVI(model, guide, optimizer, ELBO)
 
 l = 0
 for step in range(1, 501):
-   l += svi.step()
+   l += svi.step(obs)
    if step % 50 == 0:
       print(float(l))
       l = 0.
@@ -106,9 +88,5 @@ for step in range(1, 501):
 print('===')
 a = pyro.param('a')
 b = pyro.param('b')
-
-print(a)
-print(b)
-
-print(torch.distributions.log_normal.LogNormal(a,b).sample([100]).view(-1))
+print(torch.distributions.log_normal.LogNormal(a,b).sample([18]).view(-1))
 print(torch.distributions.log_normal.LogNormal(a,b).sample([1000]).view(-1).mean())
